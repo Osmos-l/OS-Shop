66d83a5406e35008c68c43c4c4de39e5f78add32
O S M O S <36885114+OsmosKesko@users.noreply.github.com>
2018-05-19 11:27:25 +0200

    Update shared.lua

--- nlf_npcshop/lua/entities/nlf_shopx/shared.lua
+++ nlf_npcshop/lua/entities/nlf_shopx/shared.lua
@@ -10,21 +10,9 @@ ENT.PrintName 	= "NPC Shop"
 ENT.Author 		= "Osmos"
 ENT.Contact 	= "https://steamcommunity.com/id/ThePsyca/"
 ENT.Category	= "NLF | Shop"
-
-ENT.AutomaticFrameAdvance = true
    
 ENT.Spawnable = true
 ENT.AdminSpawnable = true
-
-function ENT:PhysicsCollide(data, physobj)
-end
-
-function ENT:PhysicsUpdate(physobj)
-end
-
-function ENT:SetAutomaticFrameAdvance(bUsingAnim)
-	self.AutomaticFrameAdvance = bUsingAnim
-end
 
 --[[
 Addon by Osmos[FR] : https://steamcommunity.com/id/ThePsyca/
